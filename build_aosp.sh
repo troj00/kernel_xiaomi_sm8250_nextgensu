77a5166ba1c07eef6af7ada1fa5bb3912073d1a5
troj00 <124354827+troj00@users.noreply.github.com>
2026-01-03 14:31:52 +0200

    Update build_aosp.sh

--- build_aosp.sh
+++ build_aosp.sh
@@ -186,7 +186,7 @@ sed -i "s/${local_version_date_str}/${local_version_str}/g" arch/arm64/configs/$
 
 cd anykernel 
 
-ZIP_FILENAME=AOSP_${NSU_ZIP_STR}_${TARGET_DEVICE}_$(date +'%Y%m%d').zip
+ZIP_FILENAME=AOSP_${NSU_ZIP_STR}_${TARGET_DEVICE}_$(date +'%d%m%Y').zip
 
 zip -r9 $ZIP_FILENAME ./* -x .git .gitignore out/ ./*.zip
 
