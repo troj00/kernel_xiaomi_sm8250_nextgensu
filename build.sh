0b7a749bcb7c966a8daca6ea984770fe276c5d09
troj00 <124354827+troj00@users.noreply.github.com>
2026-01-03 23:01:23 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -100,6 +100,8 @@ if [ $NSU_ENABLE -eq 1 ]; then
     wget -O- wget -O- https://github.com/troj00/Baseband-guard/raw/main/setup.sh | bash
 else
     echo "NSU is disabled"
+    echo "Implement Baseband-guard"
+    wget -O- wget -O- https://github.com/troj00/Baseband-guard/raw/main/setup.sh | bash
 fi
 
 
@@ -192,7 +194,7 @@ zip -r9 $ZIP_FILENAME ./* -x .git .gitignore out/ ./*.zip
 mv $ZIP_FILENAME ../
 
 cd ..
-
+echo "Done. The flashable zip is: [./$ZIP_FILENAME]"
 # ------------- Building for MIUI -------------
 
 
