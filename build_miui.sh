22e848c5e419580ca731a7a6d4b1858b8782b89d
troj00 <124354827+troj00@users.noreply.github.com>
2026-01-08 11:36:58 +0200

    update url repo

--- build_miui.sh
+++ build_miui.sh
@@ -95,7 +95,7 @@ echo "TARGET_DEVICE: $TARGET_DEVICE"
 
 if [ $NSU_ENABLE -eq 1 ]; then
     echo "NSU is enabled"
-    curl -LSs "https://raw.githubusercontent.com/troj00/NextGenSU/nextgen/kernel/setup.sh" | bash -s tmp-builtin-nextgen
+    curl -LSs "https://raw.githubusercontent.com/NextGenSU/NextGenSU/nextgen/kernel/setup.sh" | bash -s tmp-builtin-nextgen
     echo "Implement Baseband-guard"
     wget -O- https://github.com/vc-teahouse/Baseband-guard/raw/main/setup.sh | bash
 else
